d23998dfa4f6c8c5075f1cbea582f4b346822afb
Ccccx159 <734751044@qq.com>
2023-12-25 13:48:51 +0800

    update

--- trashbin.sh
+++ trashbin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export PATH="/home/xuan/workspace/codeSpace/linux-TrashBin":$PATH
+export PATH="$HOME/.config/linux-Trashbin":$PATH
 # echo $PATH
-chmod +x /home/xuan/workspace/codeSpace/linux-TrashBin/*.sh
+chmod +x $HOME/.config/linux-Trashbin/*.sh
 
 alias del="deleteTrash.sh"
 alias res="restoreTrash.sh"
@@ -11,7 +11,7 @@ alias clt="cleanTrash.sh"
 if [[ -z `crontab -l | grep "cleanTrash.sh"` ]]; then
     # 添加定时任务
     crontab -l > /tmp/crontab.bak
-    echo "0 0 * * 0 /home/xuan/workspace/codeSpace/linux-TrashBin/cleanTrash.sh" >> /tmp/crontab.bak
+    echo "0 0 * * 0 $HOME/.config/linux-Trashbin/cleanTrash.sh" >> /tmp/crontab.bak
     crontab /tmp/crontab.bak
     rm -rf /tmp/crontab.bak
 fi
